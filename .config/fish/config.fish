88d134e1f221bb255f8306d8cf101e99077cc2c5
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-06 11:40:57 -0300

    refactor: remove unused vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
 alias g git
+alias pn pnpm
 command -qv nvim && alias vim nvim
 
 set -gx EDITOR nvim
@@ -22,23 +23,6 @@ set -gx EDITOR nvim
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
-
-# NodeJS
-set -gx PATH node_modules/.bin $PATH
-
-# Go
-set -g GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH
-
-# NVM
-function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-  status --is-command-substitution; and return
-
-  if test -f .nvmrc; and test -r .nvmrc;
-    nvm use
-  else
-  end
-end
 
 switch (uname)
   case Darwin
