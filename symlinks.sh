80b7fbbf7ea379c84a9a629de192763056482b34
Guilherme Gabriel Rigotti <guilhermerigotti@192.168.1.5>
2023-03-07 20:25:23 -0300

    refactor: move folder into root

---
+++ symlinks.sh
@@ -0,0 +1,11 @@
+f $PWD/bin ~/bin
+ln -sf $PWD/conky ~/.config/conky
+ln -sf $PWD/nvim ~/.config/nvim
+ln -sf $PWD/coc/extensions ~/.config/coc/extensions
+
+ln -sf $PWD/tmux ~/.tmux
+ln -sf $PWD/tmux/.tmux/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/tmux/.tmux.conf.local ~/.tmux.conf.local
+
+ln -sf $PWD/.config/fish ~/.config/fish
+
