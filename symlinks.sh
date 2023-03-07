c627f1ba90a42098ba2a9fe570d83f5bcd006f49
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-07 20:44:54 -0300

    feat: add new symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,11 +1,12 @@
-f $PWD/bin ~/bin
-ln -sf $PWD/conky ~/.config/conky
+#!bin/bash
+
 ln -sf $PWD/nvim ~/.config/nvim
-ln -sf $PWD/coc/extensions ~/.config/coc/extensions
 
-ln -sf $PWD/tmux ~/.tmux
-ln -sf $PWD/tmux/.tmux/.tmux.conf ~/.tmux.conf
-ln -sf $PWD/tmux/.tmux.conf.local ~/.tmux.conf.local
-
-ln -sf $PWD/.config/fish ~/.config/fish
-
+ln -sf $PWD/tmux ~/.config/tmux
+ln -sf $PWD/fish ~/.config/fish
+ln -sf $PWD/yabai/.yabairc ~/.yabairc
+ln -sf $PWD/skhdrc/.skhdrc ~/.skhdrc
+ln -sf $PWD/git/.gitconfig ~/.gitconfig
+ln -sf $PWD/default-python-packages/.default-python-packages ~/.default-python-packages
+ln -sf $PWD/asdf/.asdfrc ~/.asdfrc
+ln -sf $PWD/default-npm-packages/.default-npm-packages ~/.default-npm-packages
