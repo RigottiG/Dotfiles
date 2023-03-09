a3b9c178ea1d703f5727da8847f2850cbbec4abf
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-09 09:04:47 -0300

    refactor: enable true color support

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -8,37 +8,9 @@ end
 -- │ Setup Colorscheme                                        │
 -- ╰──────────────────────────────────────────────────────────╯
 catppuccin.setup({
-   flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = false,
-    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-    term_colors = false,
-    dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
+   flavour = "macchiato", 
+    transparent_background = true,
+    term_colors = true,
     integrations = {
         cmp = true,
         gitsigns = true,
@@ -46,6 +18,7 @@ catppuccin.setup({
         telescope = true,
         notify = false,
         mini = false,
+        treesitter = true,
     },
 })
 
